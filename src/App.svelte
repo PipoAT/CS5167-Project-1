a1ae0e807024cb774bbdbc344d682285d67a87c6
Andrew Pipo <93549755+PipoAT@users.noreply.github.com>
2024-09-14 22:59:58 -0400

    Testing vercel deployment; starting basic layout

--- src/App.svelte
+++ src/App.svelte
@@ -9,4 +9,11 @@
     <h2>It has been {daysSince} day(s) since you joined</h2>
     <h2>You have been active for {daysSince} day(s) in a row!</h2>
   </div>
+  <div id="form">
+    <form>
+      <label for="activity">What did you do today?</label>
+      <input type="text" id="activity" name="activity" required>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
 </main>
