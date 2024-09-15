9b706d3947d48e32ccce60cbb2aa052e093b3a5c
Andrew Pipo <93549755+PipoAT@users.noreply.github.com>
2024-09-14 23:13:19 -0400

    intial main form layout development

--- src/App.svelte
+++ src/App.svelte
@@ -12,9 +12,34 @@
     </div>
     <div id="form">
       <form>
-        <label for="activity">What did you do today?</label>
-        <input type="text" id="activity" name="activity" required>
-        <button type="submit">Submit</button>
+        <label for="anxiety">Did you feel anxious today?</label>
+        <input type="radio" id="anxiety-yes" name="anxiety" value="yes">
+        <label for="anxiety-yes">Yes</label>
+        <input type="radio" id="anxiety-no" name="anxiety" value="no">
+        <label for="anxiety-no">No</label>
+
+        <label for="emotions">What emoitions were you experiencing?</label>
+        <input type="text" id="emotions" name="emotions" required>
+
+        <label for="thoughts">What thoughts were you experiencing?</label>
+        <input type="text" id="thoughts" name="thoughts" required>
+
+        <label for="events">What events occured that led up to or triggered your anxiety and/or emotions?</label>
+        <input type="text" id="events" name="events" required>
+
+        <label for="time">How long did you expreince anxiety/emotions?</label>
+        <input type="text" id="time" name="time" required>
+        
+        <label for="effect">Did it effect any daily routines or tasks for the day?</label>
+        <input type="radio" id="effect-yes" name="effect" value="yes">
+        <label for="affect-yes">Yes</label>
+        <input type="radio" id="effect-no" name="effect" value="no">
+        <label for="affect-no">No</label>
+
+
+        <button type="submit">Save</button>
+        <button type="submit">Edit</button>
+        <button type="submit">Delete</button>
       </form>
     </div>
   </div>
