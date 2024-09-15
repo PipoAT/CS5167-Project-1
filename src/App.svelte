a590cb2f8d9df9eb50854622f4d0ce5f996398f5
Andrew Pipo <93549755+PipoAT@users.noreply.github.com>
2024-09-14 23:29:15 -0400

    adding more space in between info containers; ensuring minimal spacing

--- src/App.svelte
+++ src/App.svelte
@@ -8,12 +8,15 @@
       <div id="overview-sep">
         <h2>Welcome, UserName!</h2>
       </div>
+      <br>
       <div id="overview-sep">
         <h2>{currentDateOnly}</h2>
       </div>
+      <br>
       <div id="overview-sep">
         <h2>It has been {daysSince} day(s) since you joined</h2>
       </div>
+      <br>
       <div id="overview-sep">
         <h2>You have been active for {daysSince} day(s) in a row!</h2>
       </div>
