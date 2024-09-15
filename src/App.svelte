20684d817d92fd8778bd541d45559e63e9694e31
Andrew Pipo <93549755+PipoAT@users.noreply.github.com>
2024-09-14 23:15:13 -0400

    fixing issue with questions being condensed together

--- src/App.svelte
+++ src/App.svelte
@@ -17,26 +17,25 @@
         <label for="anxiety-yes">Yes</label>
         <input type="radio" id="anxiety-no" name="anxiety" value="no">
         <label for="anxiety-no">No</label>
-
+        <br>
         <label for="emotions">What emoitions were you experiencing?</label>
         <input type="text" id="emotions" name="emotions" required>
-
+        <br>
         <label for="thoughts">What thoughts were you experiencing?</label>
         <input type="text" id="thoughts" name="thoughts" required>
-
+        <br>
         <label for="events">What events occured that led up to or triggered your anxiety and/or emotions?</label>
         <input type="text" id="events" name="events" required>
-
+        <br>
         <label for="time">How long did you expreince anxiety/emotions?</label>
         <input type="text" id="time" name="time" required>
-        
+        <br>
         <label for="effect">Did it effect any daily routines or tasks for the day?</label>
         <input type="radio" id="effect-yes" name="effect" value="yes">
         <label for="affect-yes">Yes</label>
         <input type="radio" id="effect-no" name="effect" value="no">
         <label for="affect-no">No</label>
-
-
+        <br>
         <button type="submit">Save</button>
         <button type="submit">Edit</button>
         <button type="submit">Delete</button>
