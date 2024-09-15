f28eaa258d92ddafe76ff13d2bc4d366f90b737c
Andrew Pipo <93549755+PipoAT@users.noreply.github.com>
2024-09-14 23:02:33 -0400

    Adjusting layout for info and main form

--- src/App.svelte
+++ src/App.svelte
@@ -3,17 +3,19 @@
 </script>
 
 <main>
-  <div id="overview">
-    <h2>Welcome, UserName!</h2>
-    <h2>{currentDateOnly}</h2>
-    <h2>It has been {daysSince} day(s) since you joined</h2>
-    <h2>You have been active for {daysSince} day(s) in a row!</h2>
-  </div>
-  <div id="form">
-    <form>
-      <label for="activity">What did you do today?</label>
-      <input type="text" id="activity" name="activity" required>
-      <button type="submit">Submit</button>
-    </form>
+  <div class="container">
+    <div id="overview">
+      <h2>Welcome, UserName!</h2>
+      <h2>{currentDateOnly}</h2>
+      <h2>It has been {daysSince} day(s) since you joined</h2>
+      <h2>You have been active for {daysSince} day(s) in a row!</h2>
+    </div>
+    <div id="form">
+      <form>
+        <label for="activity">What did you do today?</label>
+        <input type="text" id="activity" name="activity" required>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   </div>
 </main>
