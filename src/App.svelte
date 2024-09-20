204474268246113a6b54e9bcae81b34519871aac
Andrew Pipo <93549755+PipoAT@users.noreply.github.com>
2024-09-20 14:09:32 -0400

    "Removed vite.svg file and updated image paths in App.svelte from 'src/assets' to 'public/assets', also added a disable condition to the delete button"

--- src/App.svelte
+++ src/App.svelte
@@ -79,9 +79,9 @@
     </div>
     <div id="form">
       <div id="form-date" style="display: flex; align-items: center; justify-content: center;">
-        <img src="src\assets\left-arrow.png" alt="Previous" on:click={() => updateDate(-1)} />
+        <img src="public\assets\left-arrow.png" alt="Previous" on:click={() => updateDate(-1)} />
         <h2 id="date-display-form" style="margin: 0 10px;">{currentDateSelected}</h2>
-        <img src="src\assets\right-arrow.png" alt="Next" on:click={() => updateDate(1)}/>
+        <img src="public\assets\right-arrow.png" alt="Next" on:click={() => updateDate(1)}/>
       </div>
       <br>
       <div id="form-main">
@@ -115,7 +115,7 @@
           <label for="effect-no">No</label>
           <br><br>
           <button id="saveButton" type="submit">Save</button>
-          <button id="deleteButton" on:click={() => deleteForm()}>Delete</button>
+          <button id="deleteButton" on:click={() => deleteForm()} disabled={!entry.anxiety && !entry.emotions && !entry.thoughts && !entry.events && !entry.time && !entry.effect}>Delete</button>
         </form>
       </div>
     </div>
